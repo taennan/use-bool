bbd058080b29053e6ba9d34ee5802980f31ab4f1
taennan <taennan@scoobyapps.com>
2023-12-21 15:28:29 +1200

    migrated to @testing-library/react

--- src/__tests__/useBool.test.ts
+++ src/__tests__/useBool.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react'
 import { useBool } from '../useBool'
 
 describe('useBool', () => {
